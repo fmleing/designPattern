2b2c0870dcd908581b92d14c9aff5a1abf177cd5
Mengleifeng <fmleing@163.com>
2019-12-17 19:06:16 +0800

    单线程单例模式无法适应多线程中出现的问题

---
+++ src/java/designPattern/DCLSingleton.java
@@ -0,0 +1,39 @@
+package designPattern;
+
+/**
+ * DCL版单例模式
+ */
+public class DCLSingleton {
+
+
+}
+
+/**
+ * 单线程版单例
+ */
+class SingletonOne {
+    private static SingletonOne singletonOne = null;
+
+    private SingletonOne(){
+        System.out.println(Thread.currentThread().getName() + "创建了单例");
+    }
+
+    public static SingletonOne getSingletonOne(){
+        if (singletonOne == null) {
+            singletonOne = new SingletonOne();
+        }
+        return singletonOne;
+    }
+
+}
+
+class Main{
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Thread(()->{
+                SingletonOne.getSingletonOne();
+            },String.valueOf(i).toString()).start();
+        }
+    }
+
+}
